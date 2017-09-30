20949b289a5bb9acbb30a1704a9eb57cb7bea363
Gan Ainm <gan.ainm.riomhphost@gmail.com>
2017-09-30 10:21:50 +0200

    Exercise 3, study drill 1.

--- ex3.py
+++ ex3.py
@@ -1,23 +1,37 @@
+# An introductory comment at the beginning
 print("I will now count my chickens.")
 
+# We count the hens by adding and dividing...
 print("Hens", 25 + 30 / 6)
+# ...and the roosters using subtraction, multiplication, and modulus
 print("Roosters", 100 - 25 * 3 % 4)
 
+# Introduction to the egg-counting-section
 print("Now I will count the eggs:")
 
+# Pretty complicated formula
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
 
+# An interesting mathematical problem...
 print("Is it true that 3 + 2 < 5 - 7?")
 
+# See, what Python thinks about it
 print(3 + 2 < 5 - 7)
 
+# A simple addition
 print("What is 3 + 2?", 3 + 2)
+# A simple subtraction
 print("What is 5 - 7?", 5 - 7)
 
+# Some shooting the bull..
 print("Oh, that's why it's False.")
 
+# And even more
 print("How about some more.")
 
+# Starting with greater than...
 print("Is it greater?", 5 > -2)
+# ...we use greater or equal
 print("Is it greater or equal?", 5 >= -2)
+# ...and stop with less or equal
 print("Is it less or equal?", 5 <= -2)
